1fca029f8d6ac46eba9ce6b2c7093355218f7319
Cory Pollard <astandard87@gmail.com>
2023-02-16 21:39:45 -0600

    Added bounce animation to order btn

--- src/components/UI/OrderBtn.tsx
+++ src/components/UI/OrderBtn.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './OrderBtn.module.css';
 
 const OrderBtn: React.FC = () => {
+  const [bounce, setBounce] = useState(false);
+
+  const btnBounceHandler = () => {
+    setBounce(true);
+    const timeout = setTimeout(() => {
+      setBounce(false);
+    }, 250);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  };
+
+  const orderClasses = `${classes.order} ${
+    bounce ? classes['animate-bump'] : undefined
+  }`;
+
   return (
-    <Link to="/order" className={classes.order}>
+    <Link to="/order" className={orderClasses} onClick={btnBounceHandler}>
       Your Order
       <div className={classes.badge}>4</div>
     </Link>
