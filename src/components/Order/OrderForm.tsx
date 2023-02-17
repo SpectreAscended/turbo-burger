b417b4f3023d54eb3bb3088e6b3b52fca031cfcf
Cory Pollard <astandard87@gmail.com>
2023-02-16 18:55:31 -0600

    created order form component

---
+++ src/components/Order/OrderForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Form } from 'react-router-dom';
+
+interface OrderFormProps {}
+
+const OrderForm: React.FC = () => {
+  return <Form></Form>;
+};
+
+export default OrderForm;
