03399c049b13254fcae26e5f1c22633448a0b399
Cory Pollard <astandard87@gmail.com>
2023-02-20 15:48:01 -0600

    Added helper component to scroll to top of screen whenever a new page is loaded

--- src/pages/layout/Root.tsx
+++ src/pages/layout/Root.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
+import ScrollToTop from '../../utlities/ScrollToTop';
 
 const RootLayout: React.FC = () => {
   return (
     <>
       <Header />
       <main>
+        <ScrollToTop />
         <Outlet />
       </main>
       <Footer />
