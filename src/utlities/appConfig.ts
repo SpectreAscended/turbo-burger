1c472feb288a8d3343749014aaa2458bde3db3a8
Cory Pollard <astandard87@gmail.com>
2023-02-27 23:49:13 -0600

    Added comments to clarify items in appConfig

--- src/utlities/appConfig.ts
+++ src/utlities/appConfig.ts
@@ -1,7 +1,11 @@
+// Duration for the order button bounce animation
 export const BTN_BOUNCE_DURATION = 250;
 
+// Saskatchewan sales tax
 export const SK_SALES_TAX = 0.11;
 
+// Delivery fee for ordering meals
 export const DELIVERY_FEE = 5;
 
+// Minimum limit for free meal delivery
 export const FREE_DELIVERY = 75;
