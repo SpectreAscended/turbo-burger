ef4226cd971306e675f40795714ff21b4c3f1770
Cory Pollard <astandard87@gmail.com>
2023-02-27 23:49:58 -0600

    Edited comment for SK sales tax

--- src/utlities/appConfig.ts
+++ src/utlities/appConfig.ts
@@ -1,7 +1,7 @@
 // Duration for the order button bounce animation
 export const BTN_BOUNCE_DURATION = 250;
 
-// Saskatchewan sales tax
+// Saskatchewan sales tax including both GST and PST
 export const SK_SALES_TAX = 0.11;
 
 // Delivery fee for ordering meals
