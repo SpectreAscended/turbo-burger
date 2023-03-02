e5574083a4a4129bbec2789053623ca7a033a4e7
Cory Pollard <astandard87@gmail.com>
2023-03-02 13:30:05 -0600

    Renamed Interface, deleted unused code

--- src/utlities/formatMenuData.ts
+++ src/utlities/formatMenuData.ts
@@ -1,21 +1,13 @@
-interface IMenuItem {
+interface MenuItem {
   id: string;
   title: string;
   price: number;
   description?: string;
 }
 
-interface IMenuData {
-  burgers: IMenuItem;
-  hotdogs: IMenuItem;
-  chicken: IMenuItem;
-  drinks: IMenuItem;
-  sides: IMenuItem;
-}
-
 // The raw menu data we recieve comes in as { {id, {...otherData}} }, so this function extracts the data into a single array of objects, without the nesting.
 
-const formatMenuData = (menuData: IMenuItem) => {
+const formatMenuData = (menuData: MenuItem) => {
   const formattedMenuData = Object.entries(menuData).map(item => {
     return { id: item[0], ...item[1] };
   });
