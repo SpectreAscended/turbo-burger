b98541ddb14aca3d44633e04e492b251399bafb6
Cory Pollard <astandard87@gmail.com>
2023-03-02 22:37:09 -0600

    created useValidation hook

--- src/hooks/useValidation.tsx
+++ src/hooks/useValidation.tsx
@@ -1,7 +1,39 @@
-type UseValidation = {
-  validate: () => boolean;
-};
+import { useState } from 'react';
 
-const useValidation = (validate: UseValidation) => {};
+type Validate = (a: string) => boolean;
+
+const useValidation = (validate: Validate) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [interacted, setInteracted] = useState(false);
+
+  // validate will return true or false depending on condition in function passed into hook.
+
+  const isValid = validate(enteredValue);
+
+  const hasError = interacted && !isValid;
+
+  const enteredValueHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setInteracted(false);
+    setEnteredValue(e.currentTarget.value);
+  };
+
+  const inputBlurHandler = () => {
+    setInteracted(true);
+  };
+
+  const resetValuesHandler = () => {
+    setInteracted(false);
+    setEnteredValue('');
+  };
+
+  return {
+    enteredValueHandler,
+    inputBlurHandler,
+    resetValuesHandler,
+    enteredValue,
+    interacted,
+    hasError,
+  };
+};
 
 export default useValidation;
