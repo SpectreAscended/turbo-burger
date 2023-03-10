5c936a02e2040e5ce277484a9a5a35ba4010f1e4
Cory Pollard <astandard87@gmail.com>
2023-03-10 16:25:52 -0600

    changed names in formatAPIData from formatMenuData

--- src/utlities/formatAPIData.ts
+++ src/utlities/formatAPIData.ts
@@ -9,18 +9,18 @@ interface ReviewItem {
   id: string;
   title: string;
   rating: string;
-  date: Date;
   description: string;
   userName: string;
+  date: Date;
 }
 
-// The raw menu data we recieve comes in as { {id, {...otherData}} }, so this function extracts the data into a single array of objects, without the nesting.
+// The raw API data we recieve comes in as { {id, {...otherData}} }, so this function extracts the data into a single array of objects, without the nesting.
 
-const formatAPIData = (menuData: MenuItem | ReviewItem) => {
-  const formattedMenuData = Object.entries(menuData).map(item => {
+const formatAPIData = (apiData: MenuItem | ReviewItem) => {
+  const formattedAPIData = Object.entries(apiData).map(item => {
     return { id: item[0], ...item[1] };
   });
-  return formattedMenuData;
+  return formattedAPIData;
 };
 
 export default formatAPIData;
