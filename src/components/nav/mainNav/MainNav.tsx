29f86f4d038d4d184974b4290c6c5945dce26e3d
Cory Pollard <astandard87@gmail.com>
2023-03-22 12:02:22 -0600

    Changed login button to logout button when logged in and adjusted classname and styles to be used for both

--- src/components/nav/mainNav/MainNav.tsx
+++ src/components/nav/mainNav/MainNav.tsx
@@ -1,7 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import { RootState } from '../../../store';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../../../store/authSlice';
+
 import './mainNav.scss';
 
 const MainNav: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const isAuthenticated = useSelector<RootState>(
+    state => state.auth.currentUser.isAuthenticated
+  ) as boolean;
+
+  const logoutHandler = () => {
+    dispatch(authActions.signOut());
+  };
+
   return (
     <nav className="main-nav">
       <ul className="main-nav__list">
@@ -37,14 +51,20 @@ const MainNav: React.FC = () => {
         </li>
 
         <li>
-          <NavLink
-            to="/login"
-            className={({ isActive }) =>
-              isActive ? 'main-nav__login--active' : 'main-nav__login'
-            }
-          >
-            Log in
-          </NavLink>
+          {!isAuthenticated ? (
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? 'main-nav__auth--active' : 'main-nav__auth'
+              }
+            >
+              Log in
+            </NavLink>
+          ) : (
+            <button className="main-nav__auth" onClick={logoutHandler}>
+              Log out
+            </button>
+          )}
         </li>
       </ul>
     </nav>
